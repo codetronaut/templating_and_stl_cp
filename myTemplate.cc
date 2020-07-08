7e6622692372f4aaf39792c35e65f9dd9728e628
Anmol Karn <anmol.karan123@gmail.com>
2020-07-08 20:06:26 +0530

    tweaks: did some
    Signed-off-by: Anmol Karn <anmol.karan123@gmail.com>

--- myTemplate.cc
+++ myTemplate.cc
@@ -4,20 +4,20 @@
 
 #include<bits/stdc++.h>
 
-#define loop(i,n) for (i=0; i<n; ++i)
-#define loop_k(i,k,n) for (i=k; i<=n; ++i)
-#define loop_r(i,k,n) for (i=k; i>=n; --i)
-
-#define watch(x) cout << (#x) << " is " << (x) << endl
-#define watchLoop(v) for(int i = 0; i < v.size(); i++) cout << v[i] << " \n"[i == v.size()-1];
-#define all(v) (v).begin(), (v).end()
- 
+// #define loop(i,n) for (i=0; i<n; ++i)
+// #define loop_k(i,k,n) for (i=k; i<=n; ++i)
+// #define loop_r(i,k,n) for (i=k; i>=n; --i)
+// 
+// #define watch(x) cout << (#x) << " is " << (x) << endl
+// #define watchLoop(v) for(int i = 0; i < v.size(); i++) cout << v[i] << " \n"[i == v.size()-1];
+// #define all(v) (v).begin(), (v).end()
+//  
 #define pb push_back 
 #define endl '\n'
 #define MAX 100000
 #define MOD 100000007
-#define INF numeric_limits<int>::max()
-#define INFLL numeric_limits<long long int>::max()
+// #define INF numeric_limits<int>::max()
+// #define INFLL numeric_limits<long long int>::max()
  
 typedef long long int ll;
 typedef long double ld;
@@ -31,8 +31,6 @@ int main(void)
 { 
     f_boot
  
-   //yha likh bakwass code
-   
    	ll n =7;
 	
 	//cin>>n;
